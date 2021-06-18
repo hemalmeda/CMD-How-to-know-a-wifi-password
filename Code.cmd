b94f4839e20df0bcb6f40d97a2e6c6afe980f261
hemalmeda <84007831+hemalmeda@users.noreply.github.com>
2021-06-18 12:56:20 +0530

    Create Code.cmd
    See the pictures if you don't understand the code above

---
+++ Code.cmd
@@ -0,0 +1,8 @@
+cd/
+netsh
+wlan show profile
+wlan show profile "network name" key=clear
+
+
+Next go to security settings and see the key content 
+the key content is the password of your wifi network
